dd097e6af032242ea421f07983a45b031643d41f
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-05-30 14:43:25 +0100

    Finished planes

--- fridaytest3/lib/plane.rb
+++ fridaytest3/lib/plane.rb
@@ -1,2 +1,22 @@
 class Plane
+
+	attr_accessor :status
+
+	def initialize
+		@status = "flying"
+	end
+
+	def status
+		@status
+	end
+
+	def land_in(airport)
+		airport.receive self
+		self.status = "landed"
+	end
+
+	def take_off_from(airport)
+		airport.release self
+		self.status = "flying"
+	end
 end
